3cbe21200f8f5d14b9f9b7ef0549c31c246f98ce
Eli Ruvinov <eli4056@gmail.com>
2020-01-13 14:48:14 +0200

    add interface robot_data

---
+++ OPP Game/src/dataStructure/robot_data.java
@@ -0,0 +1,39 @@
+package dataStructure;
+
+import utils.Point3D;
+/**
+ * This interface represents the game robot.   
+ * @author Eli Ruvinov
+ */
+public interface robot_data {
+	/**
+	 * @return the key of the node that this robot_data last was on.
+	 */
+	public int getSrc();
+	/**
+	 * @return the key of the node that this robot_data headed to.
+	 */
+	public int getDest();
+	/**
+	 * This function is changing the robot_data destination node.
+	 * @param dest - the key of the node which you want to be 
+	 * the next node that this robot_data will move to.
+	 */
+	public void setDest(int dest);
+	/**
+	 * @return the id of this robot_data
+	 */
+	public int getId();
+	/**
+	 * @return the score value of this robot_data.
+	 */
+	public double getValue();
+	/**
+	 * @return the speed value of this robot_data.
+	 */
+	public int getSpeed();
+	/**
+	 * @return the position (Point3D) of the robot_data.
+	 */
+	public Point3D getPos();
+}
