545f93df4855d17ecbda87aa7bec13929798a6cc
Eli Ruvinov <eli4056@gmail.com>
2020-01-13 04:28:07 +0200

    add interface fruit_data and enum fruit.

---
+++ OPP Game/src/dataStructure/fruit_data.java
@@ -0,0 +1,36 @@
+package dataStructure;
+
+import utils.Point3D;
+/**
+ * This interface represents the fruit of the game arena. 
+ * @author Eli Ruvinov
+ */
+public interface fruit_data {
+	/**
+	 * @return the position (Point3D) of the fruit_data.
+	 */
+	public Point3D getPos() ;
+	/**
+	 * This function is set this fruit_data position to be new Point3D.
+	 * @param pos - the new Point3D.
+	 */
+	public void setPos(Point3D pos);
+	/**
+	 * @return the type of the fruit_data (banana or apple).
+	 */
+	public fruits getType();
+	/**
+	 * This function is set this fruit_data type to be a fruit from enum fruits.
+	 * @param type - a fruit from enum fruits (banana or apple).
+	 */
+	public void setType(fruits type);
+	/**
+	 * @return the score value (double) that robot gets after eating this fruit_data.  
+	 */
+	public double getValue();
+	/**
+	 * This function is set this fruit_data value to be a new score value (double).
+	 * @param value - the new score that robot will get after eating this fruit_data from know.
+	 */
+	public void setValue(double value);
+}
---
+++ OPP Game/src/dataStructure/fruits.java
@@ -0,0 +1,6 @@
+package dataStructure;
+/**
+ * This enum class represents the fruits type the game arena have. 
+ * @author Eli Ruvinov
+ */
+public enum fruits { banana, apple };
