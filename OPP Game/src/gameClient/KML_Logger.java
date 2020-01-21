5becddf8c080794ab3749de39e972a8598958a91
Eli Ruvinov <eli4056@gmail.com>
2020-01-21 19:05:07 +0200

    KML_Logger with notes

--- OPP Game/src/gameClient/KML_Logger.java
+++ OPP Game/src/gameClient/KML_Logger.java
@@ -11,33 +11,59 @@ import dataStructure.FruitsType;
 import dataStructure.robot_data;
 import utils.Point3D;
 /**
+ * This class is used to make a kml file, to "see" the game at google earth. 
  * @author Eli Ruvinov
  */
 public class KML_Logger implements Runnable {
-
-	private String fileName;
-	private game_server server;
-	private String date;
+	/**
+	 * The kml output file
+	 */
 	private File OutputFile;
+	/**
+	 * The file name 
+	 */
+	private String fileName;
+	/**
+	 * The content of the kml output file
+	 */
 	private String content;
-	
-	private static final String header =
+	/**
+	 * The server of the game
+	 */
+	private game_server server;
+	/**
+	 * the stage of the game
+	 */
+	private int stage;
+	/**
+	 * String of the date + time
+	 */
+	private String date;
+	/**
+	 * The first lines in the kml output file
+	 */
+	private String header =
 			"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
 			"<kml xmlns=\"http://earth.google.com/kml/2.2\">\r\n" + 
 			"  <Document>\r\n" + 
-			"    <name> OPP Game </name>\n" +
-			"    <Style id=\"check-hide-children\">" + 
+			"    <name> OPP Game stage "+stage+" </name>\r\n" +
+			"    <Style id=\"check-hide-children\">\r\n" + 
 			"      <ListStyle>\r\n" + 
 			"        <listItemType>checkHideChildren</listItemType>\r\n" + 
 			"      </ListStyle>\r\n" + 
 			"    </Style>\r\n" + 
-			"    <styleUrl>#check-hide-children</styleUrl>";
-	
-	private static final String footer = "  </Document>\r\n" + 
-										 "</kml>";
-	// get icons from http://kml4earth.appspot.com/icons.html 
-	
-	private static final String appleIcon = 
+			"    <styleUrl>#check-hide-children</styleUrl>\r\n";
+	/**
+	 * The last lines in the kml output file
+	 */
+	private final String footer = 
+			"  </Document>\r\n" + 
+			"</kml>\r\n";
+	// Got icons from http://kml4earth.appspot.com/icons.html 
+	/**
+	 * The lines for the apple Icon
+	 */
+	private final String appleIcon = 
 			"    <Style id=\"apple\">\r\n" + 
 			"      <IconStyle>\r\n" + 
 			"        <scale>1.0</scale>\r\n" + 
@@ -45,9 +71,11 @@ public class KML_Logger implements Runnable {
 			"          <href>http://maps.google.com/mapfiles/kml/paddle/grn-blank.png</href>\r\n" + 
 			"        </Icon>\r\n" + 
 			"      </IconStyle>\r\n" + 
-			"    </Style>\n";
-	
-	private static final String bananaIcon =
+			"    </Style>\r\n";
+	/**
+	 * The lines for the banana Icon
+	 */
+	private final String bananaIcon =
 			"    <Style id=\"banana\">\r\n" + 
 			"      <IconStyle>\r\n" + 
 			"        <scale>1.0</scale>\r\n" + 
@@ -55,9 +83,11 @@ public class KML_Logger implements Runnable {
 			"          <href>http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png</href>\r\n" + 
 			"        </Icon>\r\n" + 
 			"      </IconStyle>\r\n" + 
-			"    </Style>\n";
-	
-	private static final String robotIcon = 
+			"    </Style>\r\n";
+	/**
+	 * The lines for the robot Icon
+	 */
+	private final String robotIcon = 
 			"    <Style id=\"robot\">\r\n" + 
 			"      <IconStyle>\r\n" + 
 			"        <scale>1.0</scale>\r\n" + 
@@ -66,57 +96,45 @@ public class KML_Logger implements Runnable {
 			"        </Icon>\r\n" + 
 			"      </IconStyle>\r\n" + 
 			"    </Style>\n";
-
-	private static final String placeMark = 
+	/**
+	 * The lines for a placeMark with Unknown date, icon and point
+	 */
+	private final String placeMark = 
 			"    <Placemark>\r\n" + 
 			"      <TimeStamp>\r\n" + 
 			"        <when>date</when>\r\n" + 
-			"      </TimeStamp>" +
+			"      </TimeStamp>\r\n" +
 			"      <styleUrl>#icon</styleUrl>\r\n" + 
 			"      <Point>\r\n" + 
-			"        <coordinates>(x,y)</coordinates>\r\n" + 
+			"        <coordinates>point</coordinates>\r\n" + 
 			"      </Point>\r\n" + 
-			"    </Placemark>\n";
-
-	/********************                      function implementation                                  ****************/
-	
+			"    </Placemark>\r\n";
+	/**
+	 * Constructor of a KML_Logger
+	 * @param support - the server of the game (game_server)
+	 * @param stage - the stage of the game
+	 * @throws IOException
+	 */
 	public KML_Logger(game_server support , int stage ) throws IOException {
-		this.server = support;
+		server = support;
+		this.stage = stage;
 		fileName = "data/" + stage + ".kml";
 		OutputFile = new File(fileName);
 		OutputFile.createNewFile();
 		content = header + appleIcon + bananaIcon + robotIcon;
 		writeToFile(content); 
 	}
-	
-	private void setDate() { 
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-		Date date = new Date();  
-		this.date = dateFormat.format(date);
-	}
-	
-	public void writeToFile(String string) throws IOException{
-		PrintWriter pw = new PrintWriter(OutputFile);
-		pw.println(string);
-		pw.close();
-	}
-	
-	private String addPlaceMark(Point3D p , String icon) {
-		String placeMark = KML_Logger.placeMark;
-		placeMark = placeMark.replace("(x,y)",p.x()+","+p.y());
-		placeMark = placeMark.replace("date", date);
-		placeMark = placeMark.replace("icon", icon); 
-		return placeMark;
-	}
-	
 	@Override
+	/**
+	 * This function is the action that make the kml output file
+	 */
 	public void run() {
 		while(server.isRunning()) {
 			try {
 				content += screenShot();
 				Thread.sleep(50);
 			} catch (Exception e) {
-				e.printStackTrace();
+				System.out.println(e.getMessage());
 			}
 		}
 		content += footer;
@@ -126,12 +144,24 @@ public class KML_Logger implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * This function is replace the content of output file with a given string
+	 * @param string - the new content of the output file
+	 * @throws IOException
+	 */
+	public void writeToFile(String string) throws IOException{
+		PrintWriter pw = new PrintWriter(OutputFile);
+		pw.println(string);
+		pw.close();
+	}
+	/**
+	 * @return the current situation of the game in kml format
+	 */
 	private String screenShot() {
 		String PlaceMark = "";
-		setDate(); 
 		fruit_data[] fruitsArr = server.getFruits();
 		robot_data[] robotsArr = server.getRobots();
+		setDate(); 
 		for (fruit_data fruit : fruitsArr) {
 			if(fruit.getType() == FruitsType.banana)
 				PlaceMark += addPlaceMark(fruit.getPos(), "banana");
@@ -142,5 +172,26 @@ public class KML_Logger implements Runnable {
 			PlaceMark += addPlaceMark(robot.getPos(), "robot");
 		}
 		return PlaceMark;
+	}
+	/**
+	 * This function update the date field of this KML_Logger
+	 */
+	private void setDate() { 
+		Date date = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");  
+		this.date = dateFormat.format(date);
+	}
+	/**
+	 * This function is replace the date, icon and point strings with the reals date, icon and point
+	 * @param icon - the placeMark icon
+	 * @param p - the placeMark point
+	 * @return finish placeMark
+	 */
+	private String addPlaceMark(Point3D p , String icon) {
+		String placeMark = this.placeMark;
+		placeMark = placeMark.replace("date", date);
+		placeMark = placeMark.replace("icon", icon); 
+		placeMark = placeMark.replace("point", p.x()+","+p.y());
+		return placeMark;
 	}
 }
